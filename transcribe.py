c22e050be806a853bff31af4afbc385e766db8ef
kai <leokhayim@gmail.com>
2026-06-03 15:55:38 -0300

    refactor: switch to faster whisper

--- transcribe.py
+++ transcribe.py
@@ -1,24 +1,31 @@
 import os
 import time
-import whisper
+from faster_whisper import WhisperModel
 
-model = whisper.load_model("base")
+model = WhisperModel("small", device="cpu", compute_type="int8")
 
-input_folder = r"\Transcription\audios"
-output_folder = r"\Transcription\transcriptions"
+input_folder = r"/home/input_path"
+output_folder = r"/home/output_path"
 os.makedirs(output_folder, exist_ok=True)
 
 for filename in os.listdir(input_folder):
     if filename.endswith((".mp3", ".wav", ".m4a")):
+
         audio_path = os.path.join(input_folder, filename)
         print (f"Transcribing {filename}")
 
-        start_time = time.time()
-        transcript = model.transcribe(audio_path)
-        duration = time.time() - start_time
+        start_time = time.perf_counter()
 
-        text = transcript["text"].strip()
-        text = text[0].upper() + text[1:] if text else text
+        segments, info = model.transcribe(audio_path, language="pt", vad_filter=True)
+
+        text_parts = []
+
+        for segment in segments:
+            text_parts.append(segment.text.strip())
+
+        text = " ".join(text_parts)
+
+        elapsed = time.perf_counter() - start_time
 
         output_file = os.path.splitext(filename)[0] + ".txt"
         output_path = os.path.join(output_folder, output_file)
@@ -26,4 +33,5 @@ for filename in os.listdir(input_folder):
         with open(output_path, "w", encoding="utf-8") as f:
             f.write(text)
 
-        print(f"Transcription saved to {output_file}, duration of transcription: {duration:.2f} seconds")
+        print(f"Saved transcription to {output_file}"
+            f"Transcription completed in {elapsed:.2f} s")
